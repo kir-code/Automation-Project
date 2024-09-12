6808aa323cb0f7990cf4c240152dd24ad3b98416
unknown <rskiruthika1@gmail.com>
2024-09-12 10:24:09 +0530

    New Sample Project

---
+++ src/test/java/org/git/Sample.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class Sample {
+	
+	public void method1() {
+		int  a1=10;
+		System.out.println(a1);
+		
+	}
+	public void method2() {
+		int  a2=100;
+		System.out.println(a2);
+	}
+	public void method3() {
+		int  a3=50;
+		System.out.println(a3);
+	}
+	
+
+}
